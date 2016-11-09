0929d592a644705f60c85d1f140a0e82b1adba1c
nicholasjhs <n.hughsam@gmail.com>
2016-11-09 10:56:37 -0500

    Change of ALLCHARACTERSfrom hard-coded to using string.printable library

--- caesar_modified_encryption.py
+++ caesar_modified_encryption.py
@@ -1,6 +1,8 @@
 import random
+import string
 
-ALLCHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`<,>.?/:;{}[]|'
+ALLCHARACTERS = string.printable
+# ALLCHARACTERS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+=~`<,>.?/:;{}[]|'
 
 keyRange = len(ALLCHARACTERS)
 
@@ -11,16 +13,16 @@ def generateKeys(numberOfNodes):
 		listOfKeys.append(key)
 	return listOfKeys
 
-#this method is really slow (runs in O(n^2))
+# this method is really slow (runs in O(n^2))
 def encryption(message, listOfKeys):
 	encryptedMessage = message
 	for key in listOfKeys:
 		translatedMessage = ''
 		for char in encryptedMessage:
 			if char in ALLCHARACTERS:
-				charPosition = ALLCHARACTERS.find(char) #gets the position of the character in the ALLCHARACTERS to shift it
+				charPosition = ALLCHARACTERS.find(char) # gets the position of the character in the ALLCHARACTERS to shift it
 				charPosition = charPosition + key
-				#deals with the wrap around if the charPosition is bigger than the length of ALLCHARACTERS
+				# deals with the wrap around if the charPosition is bigger than the length of ALLCHARACTERS
 				if charPosition >= keyRange:
 					charPosition = charPosition - keyRange
 				translatedMessage = translatedMessage + ALLCHARACTERS[charPosition]
@@ -37,10 +39,10 @@ def decryption(messageToDecrypt, listOfKeys):
 		translatedMessage = ''
 		for char in decryptedMessage:
 			if char in ALLCHARACTERS:
-				charPosition = ALLCHARACTERS.find(char) #gets the position of the character in the ALLCHARACTERS to shift it
+				charPosition = ALLCHARACTERS.find(char) # gets the position of the character in the ALLCHARACTERS to shift it
 				charPosition = charPosition - key
 
-				#deals with the wrap around if the charPosition is smaller than 0
+				# deals with the wrap around if the charPosition is smaller than 0
 				if charPosition < 0:
 					charPosition = charPosition + keyRange
 				translatedMessage = translatedMessage + ALLCHARACTERS[charPosition]
@@ -58,6 +60,6 @@ def main(message, nodes):
 	print "decrypted message is:", decryptedMessage
 	print "initial message and decrypted message equal:", message==decryptedMessage
 
-#modify the parameters here in order to test. The first parameter is the message we want to encrypt and the second is the # of nodes.
+# modify the parameters here in order to test. The first parameter is the message we want to encrypt and the second is the # of nodes.
 if __name__ == "__main__":
 	main('Whatever WHATEVER your MESSAGE is YOU CAN DECRYPT IT USING THISS THING!!! CAN you HNDLE @#!@#@!', 500)
