0ec99b3e317d972c6ba49277e4eb486339b5cb83
DK Do <dangkhoado259@gmail.com>
2016-11-10 11:21:01 -0500

    Modify generate_graph to start at 1

--- Onion_routing_Network/GraphGenerator.py
+++ Onion_routing_Network/GraphGenerator.py
@@ -10,15 +10,15 @@ def generate_graph(brite_file):
             for token in line.split():
                 # the wanted line will be similar to Nodes: ( 5 )
                 if token.isdigit():
-                    for i in range(0, int(token)):
+                    for i in range(1, int(token) + 1):
                         graph[i] = []
                     break
         if edge_reading:
             # the edges are stored as:
             # edgeid from_node to_node etc...
             tokens = line.split()
-            graph[int(tokens[1])].append(int(tokens[2]))
-            graph[int(tokens[2])].append(int(tokens[1]))
+            graph[int(tokens[1]) + 1].append(int(tokens[2]) + 1)
+            graph[int(tokens[2]) + 1].append(int(tokens[1]) + 1)
         if 'Edges:' in line:
             edge_reading = True
         if '\n' == line:
