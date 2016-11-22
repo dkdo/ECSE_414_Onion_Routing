0949cba756ebcdefefca3863e7d5cb648d150054
DK Do <dangkhoado259@gmail.com>
2016-11-22 13:35:13 -0500

    Format ProxyServer for PEP8

--- Onion_routing_Network/ProxyServer.py
+++ Onion_routing_Network/ProxyServer.py
@@ -11,35 +11,39 @@ class ProxyServerHandler(SocketServer.BaseRequestHandler):
     onionAddress = ('localhost', 9999)
 
     # TODO
-    def validRequest(self,request):
+    def validRequest(self, request):
             return True
 
-    def sendToOnionProxy(self,data,OnionAddress):
-        onionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        onionSocket.connect(OnionAddress)
-        onionSocket.send(data)
-        time.sleep(5 / 1000);  # something thread just delete before sending . Slowing the program down
-        onionResponse = onionSocket.recv(self.buffer)
+    def sendToOnionProxy(self, data, OnionAddress):
+        onion_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        onion_socket.connect(OnionAddress)
+        onion_socket.send(data)
+        # something thread just delete before sending. Slowing the program down
+        time.sleep(5 / 1000)
+        onion_response = onion_socket.recv(self.buffer)
         # time.sleep(50.0 / 1000.0);
-        self.request.send(onionResponse)
+        self.request.send(onion_response)
 
     def waitingThreads(self):
-        print "Number of Waiting Threads currently :",len(self.listOfThreads), "\n"
+        print ('Number of Waiting Threads currently:', len(self.listOfThreads),
+               '\n')
 
     def handle(self):
-        print "Received request from client\n"
+        print 'Received request from client\n'
         cur_thread = threading.current_thread()
         data = self.request.recv(self.buffer)
         self.listOfThreadsWaitingForResponse.append(cur_thread)
-        process = self.validRequest(data) # if message was a string then proceed to sending it to onion proxy
+        # if message was a string then proceed to sending it to onion proxy
+        process = self.validRequest(data)
 
         if (process):
-            print "The message is being transmitted to the onion proxy\n"
+            print 'The message is being transmitted to the onion proxy\n'
             self.sendToOnionProxy(data, self.onionAddress)
 
         else:
-            error = "The format is not correct, please resend\n"
+            error = 'The format is not correct, please resend\n'
             self.request.send(error)
+
 
 class ThreadedProxyServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
     pass
